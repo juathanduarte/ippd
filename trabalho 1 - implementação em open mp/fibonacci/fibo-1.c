c347d91b342b6a9155a5ee7ed2fbdfbc244be5bf
juathanduarte <juathan.cduarte@inf.ufpel.edu.br>
2023-11-25 10:50:43 -0300

    trabalho 1: bucketSort e fibonacci

---
+++ trabalho 1 - implementação em open mp/fibonacci/fibo-1.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <omp.h>
+int fib(int n) {
+  int r1, r2;
+  if (n<2) return n;
+#pragma omp task shared(r1) firstprivate(n)
+  {
+    r1=fib(n-1);
+  }
+
+#pragma omp task shared(r2) firstprivate(n)
+  {
+    r2=fib(n-2);
+  }
+
+#pragma omp taskwait
+
+  return r1+r2;
+}
+
+int main( int argc, char **argv ) {
+  int r, n = atoi(argv[1]);
+  double start, end;
+
+  start = omp_get_wtime(); 
+
+  #pragma omp parallel 
+  {
+    #pragma omp single
+      r = fib(n);
+  }
+  end = omp_get_wtime(); 
+  printf("- %d -\n",r);
+  printf("Tempo: %4.2f\n",end-start);
+
+  return 0;
+}
+
